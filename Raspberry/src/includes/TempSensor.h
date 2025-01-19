dac52e01066334ccbeda53c8aed2f6e8298a5184
Pablo Camardelli <pcamardelli@mg-group.com.ar>
2025-01-19 14:44:33 -0300

    Raspberry - Change DHT11 pin

--- Raspberry/src/includes/TempSensor.h
+++ Raspberry/src/includes/TempSensor.h
@@ -10,7 +10,7 @@ class TempSensor
 {
 private:
     /* data */
-    const RPiGPIOPin DHT_PIN = RPI_V2_GPIO_P1_40;
+    const RPiGPIOPin DHT_PIN = RPI_V2_GPIO_P1_32;
 
 public:
     TempSensor(/* args */);
